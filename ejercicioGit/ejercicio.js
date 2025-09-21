b8c69b2f1e9087ee2dd9dcfc08950d1a8a9d9998
angel-hub31 <moralesmales1@hotmail.com>
2025-09-20 20:34:42 -0500

    boton modificar a la imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,8 @@
 saludar=function(){
+    let cmpImagenSaludo;
+    cmpImagenSaludo=document.getElementById("imgSaludo");
+    cmpImagenSaludo.src="./imagenes/giphy.gif"
     console.log("Ingresa a saludar");
+    
+
 }
