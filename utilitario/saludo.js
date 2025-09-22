d6bfb8756c1e7bb8145f6090bbb068f732a51e33
angel-hub31 <moralesmales1@hotmail.com>
2025-09-22 15:44:18 -0500

    funcion mostrar texto implementada

--- utilitario/saludo.js
+++ utilitario/saludo.js
@@ -1,11 +1,28 @@
+saludar=function(){
+    //  recuperamos la caja de texto txtNombre
+    let  nombre=recuperarTexto("txtNombre");
+    //  recuperamos la caja de texto txtApellido
+    let apellido=recuperarTexto("txtApellido");
+     //  recuperamos la caja de texto txtEdad
+    let edad=recuperarTexto("txtEdad");
+    //  recuperamos la caja de texto txtEstatura
+    let estatura=recuperarTexto("txtEstatura");
+    
+    let mensajeBienvenida="Bienvenido "+nombre+" "+apellido;
+    mostrarTexto("lblResultado", mensajeBienvenida);
+}
+
+mostrarTexto=function(idComponente,mensaje){
+    let componente;
+    componente=document.getElementById(idComponente);
+    componente.innerText=mensaje;
+}
 recuperarTexto=function(idComponente){
    let componente;    
    let valorIngresado;
    componente=document.getElementById(idComponente);
-
    valorIngresado=componente.value;
    return valorIngresado;
-
 }
 recuperarInt=function(idComponente){
    let valorCaja =recuperarTexto(idComponente);
@@ -16,15 +33,4 @@ recuperarFloat=function(idComponente){
    let valorCaja =recuperarTexto(idComponente);
    let valorFlotante=parseFloat(valorCaja);
    return valorFlotante;
-}
-saludar=function(){
-    //  recuperamos la caja de texto txtNombre
-    let  nombre=recuperarTexto("txtNombre");
-    //  recuperamos la caja de texto txtApellido
-    let apellido=recuperarTexto("txtApellido");
-     //  recuperamos la caja de texto txtEdad
-    let edad=recuperarTexto("txtEdad");
-    //  recuperamos la caja de texto txtEstatura
-    let estatura=recuperarTexto("txtEstatura");
-
 }
