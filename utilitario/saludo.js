c0ff35070be685b1ee1eba3a3b5d6fd25b1c4686
angel-hub31 <moralesmales1@hotmail.com>
2025-09-22 15:20:59 -0500

    se implementan las funciones  recuperarInt  y recuperarFloat

--- utilitario/saludo.js
+++ utilitario/saludo.js
@@ -7,9 +7,24 @@ recuperarTexto=function(idComponente){
    return valorIngresado;
 
 }
+recuperarInt=function(idComponente){
+   let valorCaja =recuperarTexto(idComponente);
+   let valorEntero=parseInt(valorCaja);
+   return valorEntero;
+}
+recuperarFloat=function(idComponente){
+   let valorCaja =recuperarTexto(idComponente);
+   let valorFlotante=parseFloat(valorCaja);
+   return valorFlotante;
+}
 saludar=function(){
     //  recuperamos la caja de texto txtNombre
-  let  nombre=recuperarTexto("txtNombre");
-    //  recuperamos la caja de texto txtNombre
-   let apellido=recuperarTexto("txtApellido");
+    let  nombre=recuperarTexto("txtNombre");
+    //  recuperamos la caja de texto txtApellido
+    let apellido=recuperarTexto("txtApellido");
+     //  recuperamos la caja de texto txtEdad
+    let edad=recuperarTexto("txtEdad");
+    //  recuperamos la caja de texto txtEstatura
+    let estatura=recuperarTexto("txtEstatura");
+
 }
