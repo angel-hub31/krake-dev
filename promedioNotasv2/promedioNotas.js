e14f0189f4e0acfe4208fad3fed39dd369dbaf58
angel-hub31 <moralesmales1@hotmail.com>
2025-09-23 22:06:40 -0500

    promedioNotasv2

---
+++ promedioNotasv2/promedioNotas.js
@@ -0,0 +1,23 @@
+calcularPromedioNotas=function(){
+    let nota1=recuperarFlotante("txtNota1");
+    let nota2=recuperarFlotante("txtNota2");
+    let nota3=recuperarFlotante("txtNota3");
+    promedio=calcularPromedio(nota1,nota2,nota3);
+    resultadoFormateado=promedio.toFixed(2);
+
+    mostrarTexto("lblPromedio",resultadoFormateado);
+    if(resultadoFormateado<5 && resultadoFormateado>0){
+            mostrarTexto("lblMensaje","REPROBADO");
+           
+    }else if(resultadoFormateado>=5 && resultadoFormateado<=8){
+       mostrarTexto("lblMensaje","BUEN TRABAJO");
+
+    }else if(resultadoFormateado>8 && resultadoFormateado<=10){
+        mostrarTexto("lblMensaje","excelente");
+       
+
+    }else if(resultadoFormateado<=0){
+        mostrarTexto("lblMensaje","DATOS INCORRECTOS");
+     
+    }
+}
