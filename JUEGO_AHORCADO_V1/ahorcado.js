0545ab102b6e77b2ea6a6ded689592aea8f228ab
angel-hub31 <moralesmales1@hotmail.com>
2025-09-25 23:09:36 -0500

    se realiza la funcion ingresarLetra

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,65 +1,88 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
-esMayuscula = function(caracter) {
-if(caracter.length==0){
-    return false;
+esMayuscula = function (caracter) {
+    if (caracter.length == 0) {
+        return false;
 
-}
-let ascii=caracter.charCodeAt(0);
-return ascii >=65 && ascii<=90;
+    }
+    let ascii = caracter.charCodeAt(0);
+    return ascii >= 65 && ascii <= 90;
 }
 
-guardarPalabra=function(){
-    let palabraSecreta= ' ';
-    let mayuscula=0;
-    palabraSecreta=recuperarTexto("txtSecreta");
-    if(palabraSecreta.length !=5){
+guardarPalabra = function () {
+    let palabraSecreta = ' ';
+    let mayuscula = 0;
+    palabraSecreta = recuperarTexto("txtSecreta");
+    if (palabraSecreta.length != 5) {
         alert("La palabra debe tener 5 caracteres");
         return;
 
     }
-    for(let i=0; i <palabraSecreta.length; i++){
-        let caracter=palabraSecreta.charAt(i);
-        if(!esMayuscula(caracter)){
+    for (let i = 0; i < palabraSecreta.length; i++) {
+        let caracter = palabraSecreta.charAt(i);
+        if (!esMayuscula(caracter)) {
             mayuscula += 1;
         }
 
     }
-    if(mayuscula>0){
+    if (mayuscula > 0) {
         alert("los caracteres deben se MAYUSCULAS");
 
-    }else{
+    } else {
         alert("palabra guardada correctamente");
     }
-    mayuscula=0;
+    mayuscula = 0;
 }
-mostrarLetra=function(letra,posicion){
-    let cmpDiv ="div"+posicion;
-    mostrarTexto(cmpDiv,letra);
+mostrarLetra = function (letra, posicion) {
+    let cmpDiv = "div" + posicion;
+    mostrarTexto(cmpDiv, letra);
 
 }
-validar=function(){
-    let letrasEncontradas=0;
-    let intentos=0;
-    let coincidencias=0;
-    let errores=0;
-    letrasEncontradas=false;
+validar = function () {
+    let letrasEncontradas = 0;
+    let intentos = 0;
+    let coincidencias = 0;
+    let errores = 0;
+    letrasEncontradas = false;
     let caracter;
-    for(let i=0; i<palabraSecreta.length;i++){
-        caracter=palabraSecreta.charAt(i);
-        if(letra==caracter){
-            mostrarLetra(letra,i);
-            letrasEncontradas +=1;
-            coincidencias +=1;
-            letrasEncontradas=true;
+    for (let i = 0; i < palabraSecreta.length; i++) {
+        caracter = palabraSecreta.charAt(i);
+        if (letra == caracter) {
+            mostrarLetra(letra, i);
+            letrasEncontradas += 1;
+            coincidencias += 1;
+            letrasEncontradas = true;
 
         }
 
     }
-    if(!letrasEncontradas){
+    if (!letrasEncontradas) {
         alert("la letra no es parrte de la palabra");
-        errores +=1;
+        errores += 1;
         mostrarAhorcado();
 
     }
-    intentos +=1;
+    intentos += 1;
 }
+ingresarLetra = function () {
+    let letra;
+    letra = recuperarTexto("txtLetra");
+    if (letra.length != 1) {
+        alert("debe ingresar una sola letra");
+        return;
+
+    }
+    if (esMayuscula(letra)) {
+        validar(letra);
+        if (coincidencias == palabraSecreta.length) {
+            mostrarImagen("ahorcadoImagen", "ganador.gif");
+
+        }
+        if (intentos == 10) {
+            mostrarImagen("ahorcadoImagen", "gameOver.gif");
+        }
+    } else {
+        alert("solo se aceptam MAYUSCULAS");
+    }
+
+}
+
