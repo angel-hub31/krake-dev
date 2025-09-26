fcfa9d8f8b21f6631c2a0c8837517bb1c0dd8f81
angel-hub31 <moralesmales1@hotmail.com>
2025-09-25 22:41:11 -0500

    se realiza el paso 1

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,5 +1,34 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
 esMayuscula = function(caracter) {
-    let ascii = caracter.charCodeAt(0);
-    return ascii >= 65 && ascii <= 90; 
+if(caracter.length==0){
+    return false;
+
+}
+let ascii=caracter.charCodeAt(0);
+return ascii >=65 && ascii<=90;
+}
+
+guardarPalabra=function(){
+    let palabraSecreta= ' ';
+    let mayuscula=0;
+    palabraSecreta=recuperarTexto("txtSecreta");
+    if(palabraSecreta.length !=5){
+        alert("La palabra debe tener 5 caracteres");
+        return;
+
+    }
+    for(let i=0; i <palabraSecreta.length; i++){
+        let caracter=palabraSecreta.charAt(i);
+        if(!esMayuscula(caracter)){
+            mayuscula += 1;
+        }
+
+    }
+    if(mayuscula>0){
+        alert("los caracteres deben se MAYUSCULAS");
+
+    }else{
+        alert("palabra guardada correctamente");
+    }
+    mayuscula=0;
 }
