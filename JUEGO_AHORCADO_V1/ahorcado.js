e0ba68a9c5075d857542775f1270f2769f046076
angel-hub31 <moralesmales1@hotmail.com>
2025-09-25 22:24:39 -0500

    se abre el documento descargado y hago el primer commit del paso 0 agregando la funcion esMayuscula

---
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -0,0 +1,5 @@
+//No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+esMayuscula = function(caracter) {
+    let ascii = caracter.charCodeAt(0);
+    return ascii >= 65 && ascii <= 90; 
+}
