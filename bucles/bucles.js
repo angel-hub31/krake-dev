78e181b3b452296a821662a6a5aeef89ee9f14f5
angel-hub31 <moralesmales1@hotmail.com>
2025-10-24 21:17:24 -0500

    jwbb

---
+++ bucles/bucles.js
@@ -0,0 +1,28 @@
+probarAtributo = function () {
+
+    let arr = [1, 2, 3, 4, 2, 3, 2, 1]
+    console.log(arr.length);
+    arr.push("A");
+    arr.push("B");
+    arr.push("C");
+    arr.push("D");
+    arr.push("E");
+    console.log(arr.length);
+
+
+    mostrarTexto("lblAtributo");
+
+}
+
+
+
+
+probarAtributo1 = function () {
+
+   for(let i=1; i<=5;i++){
+  
+    console.log(i);
+   }
+    mostrarTexto("lblAtributo1");
+
+}
